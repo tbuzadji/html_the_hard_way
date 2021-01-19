526f6d71b641472eff470555e1f7004e3acc3eec
tbuzadji <tom.buzadji@gmail.com>
2021-01-19 20:24:38 +0200

    commitment

--- Tom's Coffee Shop/index.js
+++ Tom's Coffee Shop/index.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // let count = 5;
 // let hello = 6;
 // if (hello < count) {
@@ -6,11 +8,84 @@
 //     console.log("Bye")
 // }
 
-function greet() {
-    let name = document.querySelector("#name").value;
-    // alert("Вы хотите сказать что вас зовут " + name + "?? Вы серьезно? Очень странное имя у вас!");
-    console.log(name)
-}
+// function greet() {
+//     let name = document.querySelector("#name").value;
+// alert("Вы хотите сказать что вас зовут " + name + "?? Вы серьезно? Очень странное имя у вас!");
+//     console.log(name)
+// }
 // document.addEventListener("DOMCOntentLoaded", function() {
 //     document.querySelector('form').addEventListener("submit", greet);
 // });
+
+
+//-----------------------------------------ODD OR EVEN?---------------------------------------------------//
+
+
+// let x = (prompt("Write a number"));
+
+// if (x % 2 === 0)
+// {
+//     alert("Even")
+// } else { alert("Odd") }
+
+//------------------------------------LEAP year or NOT---------------------------------------------------//
+
+
+// let x = Number(prompt("Please write a year: "));
+// if (x % 4 === 0 && x % 100 != 0 || x % 400 === 0)
+// {
+//     alert("Leap year")
+// } else
+// {
+//     alert("Not a leap year")
+// }
+
+//------------------------------------------------------------------------------------------------------//
+// let num1 = prompt("Write first number");
+// const num2 = prompt("Write second number");
+
+// let example = num1 == num2;
+
+// document.querySelector("button").onclick = () =>
+// {
+//     // alert(num1 + " is a " + typeof (num1))
+//     // alert(num2 + " is a1 " + typeof parseInt((num2)))
+//     alert(Number(num1) + parseInt(num2))
+//     alert(num1 + num2)
+// }
+
+//------------------------------------------------------------------------------------------------------//
+// document.querySelector("button").onclick = function ()
+// {
+//     alert(example)
+// }
+
+//------------------------------------------------------------------------------------------------------//
+
+
+// let num1 = NaN;
+
+// alert(typeof (num1));
+
+// let n = 2;
+// let a = n++;
+// alert(a)
+
+
+
+
+//------------------------------------------------------------------------------------------------------//
+
+// Контроля Check
+// let example = confirm("Is it a number?");
+
+// document.querySelector("button").onclick = function ()
+// {
+//     if (example == true)
+//     {
+//         alert(example);
+//     } else
+//     {
+//         alert("Контроля!")
+//     }
+// } 
---
+++ udemy-react-course/index.js
@@ -0,0 +1,23 @@
+// let myName = "Tom";
+// console.log(myName);
+
+// myName = "Manu";
+// console.log(myName);
+
+// function printMyName(name)
+// {
+//     console.log(name);
+// }
+// printMyName("Tom");
+
+// const printMyName = name => console.log(name);
+// printMyName("Tom");  
+
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers, 4];
+// console.log(newNumbers);
+
+const person = {
+    name: "Max"
+};
+
